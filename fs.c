b54d291697fa8364e99b965c7246f45b1276ee31
user <user@localhost.localdomain>
2021-04-25 14:19:52 -0400

    makefile

--- fs.c
+++ fs.c
@@ -80,7 +80,45 @@ int umount_fs(const char *disk_name)
 	return 0;
 }
 
-// int fs_open(const char *name)
-// {
-	
-// }
+int fs_open(const char *name)
+{
+	return 0;
+}
+
+int fs_close(int fildes)
+{
+	return 0;
+}
+
+int fs_create(const char *name)
+{
+	return 0;
+}
+int fs_delete(const char *name)
+{
+	return 0;
+}
+int fs_read(int fildes, void *buf, size_t nbyte)
+{
+	return 0;
+}
+int fs_write(int fildes, void *buf, size_t nbyte)
+{
+	return 0;
+}
+int fs_get_filesize(int fildes)
+{
+	return 0;
+}
+int fs_listfiles(char ***files)
+{
+	return 0;
+}
+int fs_lseek(int fildes, off_t offset)
+{
+	return 0;
+}
+int fs_truncate(int fildes, off_t length)
+{
+	return 0;
+}
